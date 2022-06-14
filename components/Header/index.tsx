7742af9d009b8866b90a389b5481c2b99e5ab5aa
naimkst <najmulspark@gmail.com>
2022-06-14 15:06:04 +0600

    UI Template with Metamask done

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -8,7 +8,8 @@ import { ethers } from "ethers";
 import contracts from "../../public/MyToken.json";
 import { disconnect } from "process";
 var crypto = require("crypto");
-import * as copy from 'copy-to-clipboard';
+import * as copy from "copy-to-clipboard";
+import { ToastContainer, toast } from "react-toastify";
 
 const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] });
 
@@ -87,6 +88,23 @@ export default function Header() {
     }
   };
 
+  const copyAddress = async () => {
+    console.log("copy");
+    const addressGet = account;
+    copy(account?.toString());
+    toast.success("🦄 Copy Successfull!");
+  };
+
+  const logOut = async () => {
+    const setAddressLocal = await localStorage.removeItem(
+      "metamaskAddress"
+    );
+    const getAddressLocal = localStorage.getItem("metamaskAddress");
+    setLocalData(getAddressLocal);
+    deactivate();
+    setshow(!show);
+  }
+
   useEffect(() => {
     const setAddressLocal = localStorage.getItem("metamaskAddress");
     setLocalData(setAddressLocal);
@@ -131,7 +149,7 @@ export default function Header() {
               </Link>
             </ul>
           </div>
-          {account ? (
+          {!account ? (
             <div>
               <button
                 onClick={() => connect()}
@@ -142,7 +160,7 @@ export default function Header() {
             </div>
           ) : (
             <div onClick={() => setshow(!show)} className="relative">
-              <div className="flex items-center">
+              <div className="flex items-center hover:cursor-pointer">
                 <div>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -160,14 +178,8 @@ export default function Header() {
                   </svg>
                 </div>
                 <div>
-                  <p>0xC095...0044</p>
-                  <p className="text-[12px]">0.01 ETH</p>
-                </div>
-              </div>
-
-              <div className="absolute bg-slate-200 p-4 w-[300px] left-0 top-14 rounded-md">
-                <div>
-                  <div>My Wallet</div>
+                  <p className="text-gray-600 text-[14px] tracking-wider ">{ account.substring(0, 6) }...{ account.substring(36, 100) }</p>
+                  <p className="text-[11px] text-gray-500">0.01 ETH</p>
                 </div>
               </div>
             </div>
@@ -179,7 +191,6 @@ export default function Header() {
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-[30%]">
-              {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
@@ -213,8 +224,9 @@ export default function Header() {
                         </div>
                         <div>
                           <svg
+                            onClick={() => copyAddress()}
                             xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5 text-gray-600"
+                            className="h-5 w-5 text-gray-600 hover:cursor-pointer"
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
@@ -226,6 +238,74 @@ export default function Header() {
                               d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
                             />
                           </svg>
+                        </div>
+                      </div>
+
+                      <div className="mt-16 flex items-center gap-5 text-center justify-between">
+                        <div className="hover:cursor-pointer">
+                          <div className="">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-9 w-9 text-gray-600 text-center m-auto"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              strokeWidth={1.7}
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                              />
+                            </svg>
+                          </div>
+                          <div className="text-gray-600 text-[13px] text-center m-auto">
+                            Transaction
+                          </div>
+                        </div>
+
+                        <div className="hover:cursor-pointer">
+                          <div className="">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-9 w-9 text-gray-600 text-center m-auto"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              strokeWidth={1.7}
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
+                              />
+                            </svg>
+                          </div>
+                          <div className="text-gray-600 text-[13px] text-center m-auto">
+                            Authorization
+                          </div>
+                        </div>
+
+                        <div onClick={() => logOut()} className="hover:cursor-pointer">
+                          <div className="">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-9 w-9 text-gray-600 text-center m-auto"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              strokeWidth={1.7}
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                              />
+                            </svg>
+                          </div>
+                          <div className="text-gray-600 text-[13px] text-center m-auto">
+                            Disconnect
+                          </div>
                         </div>
                       </div>
                     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 import { ethers } from 'ethers'
 
+
 // const getLibrary = (provider: any) => {
 //   // return new Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
 //   return new ethers.providers.Web3Provider(provider)
