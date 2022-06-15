2711253028f632c8cb1e6e3cf4ee5300538f0fba
naimkst <najmulspark@gmail.com>
2022-06-15 19:22:05 +0600

    Project done base on requirement

--- pages/swap.tsx
+++ pages/swap.tsx
@@ -8,6 +8,15 @@ import { useWeb3React } from "@web3-react/core";
 const jsonRpcEndpoint =
   "https://ropsten.infura.io/v3/6780316e173b457c8ade056310790a31";
 
+const CMC_TOKEN_LIST =
+  "https://api.coinmarketcap.com/data-api/v3/uniswap/all.json";
+
+// Use the native token of the connected chain as the default input token
+const NATIVE = "NATIVE"; // Special address for native token
+
+// WBTC as the default output token
+const WBTC = "0xBdfdAc75d450154737ec1C4d8D949884F7B21ae2";
+
 export default function Swap() {
   const {
     active,
@@ -23,7 +32,14 @@ export default function Swap() {
 
       <div className=" py-20 m-auto items-center justify-center container text-center w-[25%]">
         <div className="Uniswap">
-          <SwapWidget width="100%" provider={provider} jsonRpcEndpoint={jsonRpcEndpoint} />
+          <SwapWidget
+            width="100%"
+            provider={provider}
+            jsonRpcEndpoint={jsonRpcEndpoint}
+            // tokenList={CMC_TOKEN_LIST}
+            defaultInputTokenAddress={NATIVE}
+            defaultOutputTokenAddress={WBTC}
+          />
         </div>
       </div>
     </>
